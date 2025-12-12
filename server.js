4e83a9030c9520c7302b1f2499a30850d421479d
artyom-activeloop <artyom@activeloop.dev>
2025-12-12 20:11:51 +0400

    edits

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
-var prerender = require('./lib');
-var s3Cache = require('prerender-aws-s3-cache');
+const prerender = require('./lib');
+const s3Cache = require('prerender-aws-s3-cache');
 
-var server = prerender();
+const server = prerender({
+    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
+    forwardHeaders: true,
+    chromeLocation: '/usr/bin/chromium-browser'
+});
+
 
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
